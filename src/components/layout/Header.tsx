bc52d5abe5c9e10c9eb6ee6ed6a001f496e8a019
AdiKol1 <koladi1@gmail.com>
2025-05-16 12:12:54 -0400

    Fix header navigation: Change Start Assessment button to link to /assessment

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -38,7 +38,7 @@ export const Header: React.FC = () => {
               Log in
             </Link>
             <Link
-              to="/start-assessment"
+              to="/assessment"
               className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition-colors"
             >
               Start Assessment
